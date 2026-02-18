52169fecb8ad08c2df54144d2c6700b97ad12b35
alfatih <muhammadalfatihwibowo8@gmail.com>
2026-02-18 17:56:54 +0700

    added deatil and related page

--- marketProject/urls.py
+++ marketProject/urls.py
@@ -1,13 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from marketApp.views import index, contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('items/', include('item.urls')),
     path('contact/', contact, name='contact'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
